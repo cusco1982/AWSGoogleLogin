4c08600a5b1177e7ea0d519a1dc79322b26fcb1f
Julian <cusco2891@gmail.com>
2019-12-12 04:40:01 -0500

    form needs work

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -18,15 +18,34 @@ class Main extends Component {
                     initialValues={{ fullname: "John Snow"}} 
                     onSubmit={(values) => console.log('submitted values= ',values)}>
                         {(props) => (<form onSubmit={props.handleSubmit}>
-                            <input type='text' name='fullname' value={props.values.fullname} placeholder='Full name' />
+                            <input 
+                            type='text' name='fullname' 
+                            value={props.values.fullname} 
+                            onChange={props.handleChange}
+                            placeholder='Full name'/>
                             <br />
-                            <input type='email' name='email' placeholder='Enter email' />
+                            <input 
+                            type='email' name='email' 
+                            value={props.values.email} 
+                            onChange={props.handleChange}
+                            placeholder='Enter email' />
                             <br />
-                            <input type='password' name='password' placeholder='Enter password' />
+                            <input 
+                            type='password' name='password' 
+                            value={props.values.password} 
+                            onChange={props.handleChange}
+                            placeholder='Enter password' />
                             <br />
-                            <input type='radio' name='gender' /> <label>Male</label>
-                            <input type='radio' name='gender' /> <label>Female</label>
+
+                            <input type='radio'
+                            onChange={props.handleChange}
+                            name='gender' value='male' /> <label>Male</label>
+
+                            <input type='radio' 
+                            onChange={props.handleChange}
+                            name='gender' value='femail' /> <label>Female</label>
                             <br />
+
                             <input type='checkbox' name='terms' /> <label>Terms&Conditions</label>
                             <br />
                             <button type='submit'>Submit</button>
