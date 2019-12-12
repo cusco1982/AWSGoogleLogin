a96b1ad430588ed797c284a4286ab15e4cd492bf
Julian <cusco2891@gmail.com>
2019-12-12 04:10:57 -0500

    spacing

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -6,16 +6,29 @@ class Main extends Component {
     render() {
         return (
             <div className='main-container'>
-            
-                <div>
-                    <h1>Login Here</h1>
-                </div>
-
-
 
                 <div className="profile-link">
                     <Link className="profile-link" to="/profile">Profile</Link>
                 </div>
+
+                <div className="login-container">
+                    <h1>Login Here</h1>
+                    <form>
+                        <input type='text' name='fullname' placeholder='Full name' />
+                        <br />
+                        <input type='email' name='email' placeholder='Enter email' />
+                        <br />
+                        <input type='password' name='password' placeholder='Enter password' />
+                        <br />
+                        <input type='radio' name='gender' /> <label>Male</label>
+                        <input type='radio' name='gender' /> <label>Female</label>
+                        <br />
+                        <input type='checkbox' name='terms' /> <label>Terms&Conditions</label>
+                        <br />
+                        <button type='submit'>Submit</button>
+                    </form>
+                </div>
+
 
             </div>
 
