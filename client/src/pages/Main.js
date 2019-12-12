c038f55f9635449b0c1aa7a1cbac4cb76ca4c4d0
Julian <cusco2891@gmail.com>
2019-12-12 04:17:40 -0500

    formik correct

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Formik } from "formik";
 
 
 class Main extends Component {
@@ -13,20 +14,23 @@ class Main extends Component {
 
                 <div className="login-container">
                     <h1>Login Here</h1>
-                    <form>
-                        <input type='text' name='fullname' placeholder='Full name' />
-                        <br />
-                        <input type='email' name='email' placeholder='Enter email' />
-                        <br />
-                        <input type='password' name='password' placeholder='Enter password' />
-                        <br />
-                        <input type='radio' name='gender' /> <label>Male</label>
-                        <input type='radio' name='gender' /> <label>Female</label>
-                        <br />
-                        <input type='checkbox' name='terms' /> <label>Terms&Conditions</label>
-                        <br />
-                        <button type='submit'>Submit</button>
-                    </form>
+                    <Formik>
+                        
+                        <form>
+                            <input type='text' name='fullname' placeholder='Full name' />
+                            <br />
+                            <input type='email' name='email' placeholder='Enter email' />
+                            <br />
+                            <input type='password' name='password' placeholder='Enter password' />
+                            <br />
+                            <input type='radio' name='gender' /> <label>Male</label>
+                            <input type='radio' name='gender' /> <label>Female</label>
+                            <br />
+                            <input type='checkbox' name='terms' /> <label>Terms&Conditions</label>
+                            <br />
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </Formik>
                 </div>
 
 
